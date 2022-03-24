3d2483a49fc69398fda27e7f2b35fb9ee0636ca1
Dmitriy <osendiesesein@gmail.com>
2022-03-24 17:03:21 +0300

    j

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+const newTask = document.body.querySelector('#placeholder')
+console.log(newTask)
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
+        // document.querySelector('#newtask').innerHTML += `
+        //     <div class="red">
+        //         <p> Вы должны написать что-то!</p> 
+        //     </div>
+        //     `
+        alert('Введите что-то')
     }
     else{
         document.querySelector('#tasks').innerHTML += `
@@ -22,3 +29,13 @@ document.querySelector('#push').onclick = function(){
         }
     }
 }
+const addedTask = document.body.querySelector('#newtask');
+fetch('http://localhost:3000/tasks/create', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(addedTask)
+})
+    .then( response => { response.json() })
+    .then( json => {
+        console.log(json)
+    })
